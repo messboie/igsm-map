b6ef8937146fb5ab80f9327531a5694bbaf91223
Finn Boie <finn@hannover-uni.de>
2021-02-03 16:58:04 +0100

    test

---
+++ igsm.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="igsm" tilewidth="239" tileheight="133" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <properties>
+  <property name="Bild" type="file" value="igsmlogo.png"/>
+ </properties>
+ <tile id="0">
+  <image width="239" height="133" source="igsmlogo.png"/>
+ </tile>
+</tileset>
---
+++ mapUtilities.tsx
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="mapUtilities" tilewidth="32" tileheight="32" tilecount="9" columns="9">
+ <image source="../world-map-starterkit/tiles_png/community/cert/imgs/tilesheets/mapUtilities.png" width="288" height="32"/>
+ <tile id="0">
+  <properties>
+   <property name="exitUrl" value="foo.json#bar"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="startLayer" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="silent" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="openWebsite" value="https://streaming.media.ccc.de"/>
+  </properties>
+ </tile>
+</tileset>
